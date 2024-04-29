1cc05f4c3fcd0a2e1443823986ebe488684d7a81
Dora <dora.nacinovic99@gmail.com>
2024-04-29 19:39:03 +0200

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   const htmlContent = `
 
-    <h1>Let's deploy this cat!!!</h1>
+    <h1>Let's deploy this cat!</h1>
     <img src="https://media.licdn.com/dms/image/D5612AQEqY7vY1zRYEg/article-cover_image-shrink_600_2000/0/1705030463020?e=2147483647&v=beta&t=pQdFr52qTcXq6iF4TibMYFhWP_s59iuXR_5_GpFmHIA" alt="cat on plane"> `
         ;
   res.send(htmlContent);
