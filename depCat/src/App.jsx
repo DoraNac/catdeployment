cb4b3edfc51a9afa071b6f1f0fdcf9634fb19ade
Dora <dora.nacinovic99@gmail.com>
2024-04-29 14:29:56 +0200

    cat

---
+++ depCat/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <img src="https://media.licdn.com/dms/image/D5612AQEqY7vY1zRYEg/article-cover_image-shrink_600_2000/0/1705030463020?e=2147483647&v=beta&t=pQdFr52qTcXq6iF4TibMYFhWP_s59iuXR_5_GpFmHIA" alt="cat on plane" />
+      </div>
+      <h1>Let's deploy this cat</h1>
+    </>
+  );
+}
+
+export default App;
